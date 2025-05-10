747567ae3b1ec9838fcc2ac00fff4d6099edd7f2
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-05-09 19:54:54 -0500

    Update count_minutes function call to use 30 minutes

--- src/scrapy_check_updates.py
+++ src/scrapy_check_updates.py
@@ -95,7 +95,7 @@ def delete_old_file(filepath: str) -> None:
         logger.info(f"File not found, nothing to delete: {filepath}")
 
 
-# count_minutes(60)
+count_minutes(30)
 setup_directory(__file__)
 
 if not os.path.exists("old.jsonl"):
