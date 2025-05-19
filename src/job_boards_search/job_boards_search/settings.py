5b4c3686b567808b5fd6ae97f10927bf2c4d9ddc
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-05-19 18:45:27 -0500

    Add custom user agent to Scrapy settings for improved request handling

--- src/job_boards_search/job_boards_search/settings.py
+++ src/job_boards_search/job_boards_search/settings.py
@@ -101,3 +101,8 @@ FEED_EXPORT_ENCODING = "utf-8"
 #     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 # }
 # TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/90.0.4430.93 Safari/537.36"
+)
